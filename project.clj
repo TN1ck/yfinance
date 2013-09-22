04d46b2c6da4ede5a61db72cf726c2abca0c96d1
TN1ck <Tom.w.a.nick@gmail.com>
2013-09-22 15:49:30 +0200

    added clojars and url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject yfinance "0.3.0-SNAPSHOT"
+(defproject org.clojars.tn1ck/yfinance "0.3.0-SNAPSHOT"
   :description "Clojure code to access Yahoo! Finance"
+  :url "https://github.com/TN1ck/yfinance"
   :author "Baishampayan Ghose"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
