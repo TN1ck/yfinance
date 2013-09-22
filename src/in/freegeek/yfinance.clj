6503447012ddd80e8e80d9f4717a9e909fdbfddd
TN1ck <Tom.w.a.nick@gmail.com>
2013-09-22 15:27:59 +0200

    fetch-historical-data works now with symbols instead of strings, more ideomatic

--- src/in/freegeek/yfinance.clj
+++ src/in/freegeek/yfinance.clj
@@ -12,9 +12,9 @@
   [y1 m1 d1 y2 m2 d2 p sym]
   (let [start (time/date-time y1 m1 d1)
         end (time/date-time y2 m2 d2)
-        period (get {:daily "d"
-                     :weekly "w"
-                     :monthly "m"} p)]
+        period (p {:daily "d"
+                   :weekly "w"
+                   :monthly "m"})]
     (format +base-url+
             (URLEncoder/encode sym "UTF-8")
             (dec (time/month start))
@@ -34,18 +34,20 @@
   "Wait for all the agents to finish and then return the response"
   [& responses]
   (for [response responses]
-    (if (= (:status response) 200)
-      (:body response)
-      (:status response))))
+    (let [{:keys [status body]} response]
+      (if (= status 200)
+        body
+        response))))
 
 (defn fetch-historical-data
-  "Fetch historical prices from Yahoo! finance for the given symbols between start and end"
+  "Fetch historical prices from Yahoo! finance for the given symbols between start and end
+   example-usage: (fetch-historical-data \"2009-01-01\" \"2009-01-15\" [:AAPL :IBM])"
   ([start end syms] (fetch-historical-data start end syms :daily))
   ([start end syms period]
      (letfn [(parse-date [^String dt] (map #(Integer/parseInt %) (.split dt "-")))]
        (let [[y1 m1 d1] (parse-date start)
              [y2 m2 d2] (parse-date end)
-             urls (map (partial get-full-url y1 m1 d1 y2 m2 d2 period) syms)
+             urls (map (partial get-full-url y1 m1 d1 y2 m2 d2 period) (map name syms))
              ;; worth multi-threading since network should be bottle-neck
              responses (pmap fetch-url urls)]
          (zipmap syms (apply collect-response responses))))))
