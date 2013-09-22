3b1e877911801e75ff3ad879fb0ba550be1f30b3
TN1ck <Tom.w.a.nick@gmail.com>
2013-09-22 15:27:09 +0200

    updated to newer versions of packages

--- project.clj
+++ project.clj
@@ -1,11 +1,6 @@
-(defproject yfinance "0.2.0-SNAPSHOT"
+(defproject yfinance "0.3.0-SNAPSHOT"
   :description "Clojure code to access Yahoo! Finance"
   :author "Baishampayan Ghose"
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [clj-time "0.3.3"]
-                 [clj-http "0.2.4"]]
-
-  :plugins [[s3-wagon-private "1.1.1"]]
-
-  :repositories {"nfr-releases" "s3p://newfound-mvn-repo/releases/"
-                 "nfr-snapshots" "s3p://newfound-mvn-repo/snapshots/"})
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-time "0.6.0"]
+                 [clj-http "0.7.7"]])
