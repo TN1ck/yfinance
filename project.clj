b871255b49e0c17853fa2fbadbf200dcad26263b
TN1ck <tomwanick@gmail.com>
2013-10-26 12:11:34 +0200

    changed versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.tn1ck/yfinance "0.4.0-SNAPSHOT"
+(defproject org.clojars.tn1ck/yfinance "0.4.2-SNAPSHOT"
   :description "Clojure code to access Yahoo! Finance"
   :url "https://github.com/TN1ck/yfinance"
   :author "Baishampayan Ghose"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.6.0"]
-                 [clj-http "0.7.7"]
+                 [clj-http "0.7.6"]
                  [org.clojure/data.csv "0.1.2"]])
